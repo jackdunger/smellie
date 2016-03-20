9c41800741fe1c0acc0f3efd8a27b056651dadd5
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2016-03-19 21:03:23 -0400

    no hack laserSwitch

--- software/laserSwitch.py
+++ software/laserSwitch.py
@@ -3,54 +3,42 @@
 # Additional changes by Krish Majumdar (24/01/2013, 05/03/2013)
 # This requires the LabJackPython Software that has been adapted to work on a Windows 7 64-bit machine: https://github.com/labjack/LabJackPython
 # Please check the u12SMELLIE.py script for more details
-
-import u12SMELLIE, time
-d = u12SMELLIE.U12()
+import sys
+import time
+sys.path.append("../LabJackPython/src")
+from u12 import U12
+d = U12()
 
 
 # Increment the selected channel by 1 ... pulse the channel-up bit on the LabJack
 def ChannelUp():    
-    d.eDigitalOut(1,1)	# Raises D1
-    d.eDigitalOut(1,0)	# Lowers D1
-    d.eDigitalOut(1,1)	# Raises D1
+    d.eDigitalOut(1, 1, writeD = 1)	# Raises D1
+    d.eDigitalOut(1, 0, writeD = 1)	# Lowers D1
+    d.eDigitalOut(1, 1, writeD = 1)	# Raises D1
 
 
 # Execute and switch the active channel to the selected one ... pulse the execute-bit on the LabJack
 def Execute():    
-    d.eDigitalOut(0,1)	# Raises D0
-    d.eDigitalOut(0,0)	# Lowers D0
-    d.eDigitalOut(0,1)	# Raises D0
+    d.eDigitalOut(0, 1, writeD = 1)	# Raises D0
+    d.eDigitalOut(0, 0, writeD = 1)	# Lowers D0
+    d.eDigitalOut(0, 1, writeD = 1)	# Raises D0
     time.sleep(30)      # The SEPIA Unit needs a short amount of time to fully power up 
 
+# invert a boolean input
+def invert(bit):
+    if bit in (0, 1):
+        return (not bit)
+    return "Laser Switch (Get Selected Channel) - Invalid input ... check connections to and from the Laser Switch."
+		    
 
 # Return the channel as shown on the Laser Switch front panel's 8-segment display
 def GetSelectedChannel():
-    def invert(bit):
-        if (bit == 0):
-            bit = 1
-            return bit
-        elif(bit == 1):
-            bit = 0
-            return bit 
-        else:
-            return "Laser Switch (Get Selected Channel) - Invalid input ... check connections to and from the Laser Switch."
-		    
     channel = invert(d.eDigitalIn(2)) + (2.0 * float(invert(d.eDigitalIn(3)))) + (4.0 * float(invert(d.eDigitalIn(4))))
     return int(channel)
 
 
 # Return the currently active channel as shown on the Laser Switch front panel's LEDs
 def GetActiveChannel():
-    def invert(bit):
-        if (bit == 0):
-            bit = 1
-            return bit
-        elif(bit == 1):
-            bit = 0
-            return bit 
-        else:
-            return "Laser Switch (Get Active Channel) - Invalid input ... check connections to and from the Laser Switch."
-		    
     channel = invert(d.eDigitalIn(5)) + (2.0 * float(invert(d.eDigitalIn(6)))) + (4.0 * float(invert(d.eDigitalIn(7))))
     return int(channel)
 
